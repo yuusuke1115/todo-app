88f6748b87acc540c4cb22f87c8f96f71e64eb83
yuusuke <xiangzhongyoufu@gmail.com>
2024-02-05 13:59:52 +0900

    add: DBから値を取得できるようにした

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
+"use strict";
+
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 const PORT = 3000;
-
-const todos = [];
 
 app.set("view engine", "ejs");
 
+const pool = mysql.createPool({
+  connectionLimit: process.env.CONNECTION_LIMIT, 
+  host: process.env.HOST, 
+  user: process.env.USER, 
+  password: process.env.PASSWORD, 
+  database: process.env.DATABASE, 
+  port: process.env.PORT, 
+});
+
 app.get("/", (req, res) => {
-  res.render("home", {todos: todos});
-})
+  pool.getConnection((err, connection) => {
+    if (err) {
+      throw err;
+    };
+    connection.query("SELECT * FROM task", (err, tasks) => {
+      connection.release();
+      console.log(tasks);
+      if (!err) {
+        res.render("home", {tasks: tasks});
+      };
+    });
+  });
+});
 
 app.listen(PORT, () => {
   console.log("server is running!")
