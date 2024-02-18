5f9b457525022c468b8c23ea42fd3c9fb0b8a012
yuusuke <xiangzhongyoufu@gmail.com>
2024-02-19 06:21:28 +0900

    add: 削除機能を実装した

--- routes/home.js
+++ routes/home.js
@@ -48,7 +48,36 @@ router.post("/task/:id", (req, res) => {
     if (err) {
       throw err;
     };
-    connection.query("UPDATE task SET name = ? WHERE id = ?", [req.body.task, req.params.id], (err) => {
+    connection.query("UPDATE task SET name = ? WHERE id = ?", [req.body.updatedTask, req.params.id], (err) => {
+      connection.release();
+      if (!err) {
+        res.redirect("/home");
+      };
+    });
+  });
+});
+
+router.get("/delete", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      throw err;
+    };
+    connection.query("SELECT id, name FROM task WHERE id = ?", [req.query.id], (err, tasks) => {
+      connection.release();
+      console.log(req.query.id);
+      if (!err) {
+        res.json({tasks: tasks});
+      };
+    });
+  });
+});
+
+router.post("/delete", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      throw err;
+    };
+    connection.query("DELETE FROM task WHERE id = ?", [req.query.id], (err) => {
       connection.release();
       if (!err) {
         res.redirect("/home");
